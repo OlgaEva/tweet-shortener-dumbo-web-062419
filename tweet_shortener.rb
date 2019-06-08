7b1217958ad1b3011c9f77b0bf6d27365a3c6c38
Olga Rosas <olga.rosas@gmail.com>
2019-06-08 12:02:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+require 'pry'
+
+def dictionary # Write your code here.
+  dictionary_hash = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+new_array = []
+string.split(" ").each do |word|
+    if dictionary.keys.include?(word.downcase)
+      new_array << word = dictionary[word.downcase]
+    else
+      new_array << word
+    end
+  end
+  new_array.join(" ")
+end
+
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+def selective_tweet_shortener(tweet)
+  if tweet.size > 140
+    puts word_substituter(tweet)
+  else
+    tweet
+    end
+  end
