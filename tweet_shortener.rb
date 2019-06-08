3c10a7d7bad491752001eca6a3ce15830e43c175
Olga Rosas <olga.rosas@gmail.com>
2019-06-08 12:46:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,7 +46,7 @@ def selective_tweet_shortener(tweet)
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
     word_substituter(tweet)[0..140] + '...'
-  else 
+  else
     tweet
     end
   end
